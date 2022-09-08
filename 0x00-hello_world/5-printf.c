72c9083ca7f6918ba16d7d64b404cb45f941e685
Daniel Alonge <alongedaniel19@gmail.com>
2022-09-08 12:51:51 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,17 +2,19 @@
 
 /**
  *
- *  * main - main block
+ *main - Entry point
  *
- *   * Return: 0
+ *Description: Program description
  *
- *    */
+ *Return: Always 0 (Success)
+ *
+ */
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 
