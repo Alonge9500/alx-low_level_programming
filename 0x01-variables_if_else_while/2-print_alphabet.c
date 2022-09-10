0a7f385c01827767aa08fde1e01875f3bc6ac779
Daniel Alonge <alongedaniel19@gmail.com>
2022-09-10 14:15:59 -0700

    print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main: insomia
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lett ='a';
+
+	while (lett <= 'z')
+	{
+		putchar(lett);
+		lett++;
+	}
+	putchar('\n');
+	
+	return (0);
+}
